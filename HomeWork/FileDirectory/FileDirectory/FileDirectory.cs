954afde94f1d1f37eb8a32c978dc6079b275fabb
KOLESNICHENKO ILYA <riicray@gmail.com>
2023-08-29 22:46:41 +0300

    create and push project

---
+++ HomeWork/FileDirectory/FileDirectory/FileDirectory.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+
+namespace Otus.HomeWork.FileDirectory
+{
+    public class FileDirectory
+    {
+        private readonly DirectoryInfo _directoryInfo;
+
+        public FileDirectory(string path)
+        {
+            _directoryInfo = new DirectoryInfo(path);
+        }
+
+        public void CreateDirectory()
+        {
+            if (!_directoryInfo.Exists)
+            {
+                _directoryInfo.Create();
+            }
+
+            string[] logValue = new string[]
+            {
+                $"Название каталога: {_directoryInfo.Name}",
+                $"Полное название каталога: {_directoryInfo.FullName}",
+                $"Время создания каталога: {_directoryInfo.CreationTime}\n"
+            };
+
+            Console.WriteLine(string.Join('\n', logValue));
+        }
+
+        public void CreateSubdirectory(string path)
+        {
+            var dir = _directoryInfo.CreateSubdirectory(path);
+
+            string[] logValue = new string[]
+            {
+                $"Название подкаталога: {dir.Name}",
+                $"Полное название каталога: {dir.FullName}",
+                $"Время создания каталога: {dir.CreationTime}\n"
+            };
+
+            Console.WriteLine(string.Join('\n', logValue));
+        }
+
+        public void CreateFile(string path, string[] text)
+        {
+            try
+            {
+                File.WriteAllLines(path, text, Encoding.UTF8);
+
+                Console.WriteLine("Файл: {0}", path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        public string ReadAllText(string path)
+        {
+            return File.ReadAllText(Path.Combine(path));
+        }
+    }
+}
---
+++ HomeWork/FileDirectory/FileDirectory/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+using System.Runtime;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Otus.HomeWork.FileDirectory
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var mainPath = @"C:\Otus";
+            var path = Path.Combine(mainPath);
+
+            var fileDirectory = new FileDirectory(path);
+
+            Console.WriteLine("\n# Создание католога:\n");
+
+            fileDirectory.CreateDirectory();
+
+            Console.WriteLine("\n# Создание подкатологов:\n");
+
+            fileDirectory.CreateSubdirectory("Test1");
+            fileDirectory.CreateSubdirectory("Test2");
+
+            Console.WriteLine("\n# Создание файлов:\n");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                var name = $"File{i}.txt";
+
+                fileDirectory.CreateFile(Path.Combine(path, "Test1", name), new[] { name });
+                fileDirectory.CreateFile(Path.Combine(path, "Test2", name), new[] { name });
+            }
+
+            Console.WriteLine("\n# Добавление данных в файл:\n");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                var name = $"File{i}.txt";
+                var dt = DateTime.Now.ToString();
+
+                var textFromTestFirst = fileDirectory.ReadAllText(Path.Combine(path, "Test1", name));
+                var textFromTestSecond = fileDirectory.ReadAllText(Path.Combine(path, "Test2", name));
+
+                fileDirectory.CreateFile(Path.Combine(path, "Test1", name), new[] { textFromTestFirst, dt });
+                fileDirectory.CreateFile(Path.Combine(path, "Test2", name), new[] { textFromTestSecond, dt });
+            }
+
+            Console.WriteLine("\nЧтенеие файла:\n");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                var name = $"File{i}.txt";
+                var dt = DateTime.Now.ToString();
+
+                var textFromTestFirst = fileDirectory.ReadAllText(Path.Combine(path, "Test1", name));
+                var textFromTestSecond = fileDirectory.ReadAllText(Path.Combine(path, "Test2", name));
+
+                Console.WriteLine($"{Path.Combine(path, "Test1", name)}:\n{textFromTestFirst}");
+                Console.WriteLine($"{Path.Combine(path, "Test2", name)}:\n{textFromTestSecond}");
+            }
+        }
+    }
+}
